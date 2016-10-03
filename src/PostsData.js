c478850d7d78bc32d95a347b48f5d3384507726d
Khuong <t4chng00@students.oamk.fi>
2016-10-04 00:53:24 +0300

    ver4

--- src/PostsData.js
+++ src/PostsData.js
@@ -2,36 +2,35 @@ var posts = [
         {
             "id": 0,
             "user": {
-                "id": 1,
-                "username": "Kimmo",
-                "profileImages": "img/max.png"
+                  "id": 1,
+                  "username": "Kimmo",
+                  "profileImages": "img/max.png"
             },
+
             "image": "img/max.png",
             "imageThumb": "img/max.png",
             "likes": 1320,
-            "caption": "my new style",
             "comments": [
                 {
                     "id": 0,
                     "user": {
                         "id": 2,
                         "username": "Pyhtio"
-                    },
-                    "comment": "nothing",
-                    "userRefs": [],
                 },
+                    "comment": "nothing",
+                },
+
                 {
                     "id": 1,
                     "user": {
                         "id": 3,
                         "username": "Tapio"
-                    },
+                },
                     "comment": "thanks",
-                    "userRefs": ["Kimmo"],
-                    "tags": []
                 }
             ]
         },
+
         {
                 "id": 1,
             "user": {
@@ -42,11 +41,60 @@ var posts = [
             "image": "img/ben.png",
             "imageThumb": "img/ben.png",
             "likes": 210,
-            "caption": "I am legend",
             "comments": [
+              {
+                  "id": 2,
+                  "user": {
+                      "id": 4,
+                      "username": "Pyhtio"
+              },
+                  "comment": "nothing",
+              },
+
+              {
+                  "id": 3,
+                  "user": {
+                      "id": 5,
+                      "username": "Tapio"
+              },
+                  "comment": "thanks",
+              }
 
             ]
+        },
+
+        {
+            "id": 2,
+            "user": {
+                  "id": 3,
+                  "username": "Kimmo",
+                  "profileImages": "img/max.png"
+            },
+
+            "image": "img/max.png",
+            "imageThumb": "img/max.png",
+            "likes": 1320,
+            "comments": [
+                {
+                    "id": 4,
+                    "user": {
+                        "id": 6,
+                        "username": "Pyhtio"
+                },
+                    "comment": "nothing",
+                },
+
+                {
+                    "id": 5,
+                    "user": {
+                        "id": 7,
+                        "username": "Tapio"
+                },
+                    "comment": "thanks",
+                }
+            ]
         }
+
     ]
 
 exports.getPosts = function(){
